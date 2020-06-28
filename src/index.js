5fee59bb7790313e20528f1e7801e14002aad2cd
Brandon Rose <brandonrose805@gmail.com>
2020-06-27 21:06:14 -0700

    add animations

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './style/index.scss';
+import './style/animations.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
