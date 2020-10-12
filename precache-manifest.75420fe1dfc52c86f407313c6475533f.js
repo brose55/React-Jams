0ca4e1acf5632a869184b28930f5796976916db2
Brandon Rose <brandonrose805@gmail.com>
2020-10-12 14:13:37 -0700

    Updates

---
+++ precache-manifest.75420fe1dfc52c86f407313c6475533f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7bd7fe4fbca5f9825501c3fd76e7e1",
+    "url": "/React-Jams/index.html"
+  },
+  {
+    "revision": "a0c448a69bc5f1640154",
+    "url": "/React-Jams/static/css/main.4ef17b58.chunk.css"
+  },
+  {
+    "revision": "79411359a644373d86bd",
+    "url": "/React-Jams/static/js/2.dc5afffe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Jams/static/js/2.dc5afffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0c448a69bc5f1640154",
+    "url": "/React-Jams/static/js/main.d6caefb3.chunk.js"
+  },
+  {
+    "revision": "0c71c0279fcdcb795c54",
+    "url": "/React-Jams/static/js/runtime-main.6fabea93.js"
+  },
+  {
+    "revision": "bf658d27a56031b445ef714baaeb05f6",
+    "url": "/React-Jams/static/media/heart-beat.bf658d27.png"
+  }
+]);
